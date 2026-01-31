112234c2b057bc26e6c0287236db4be2e0e6c707
yeabsiraamare <yeabsiraamaree@gmail.com>
2026-01-31 03:39:37 +0300

    Fix librarian query to match ALX checker expected pattern

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -18,8 +18,8 @@ def get_books_in_library(library_name):
 
 def get_librarian_for_library(library_name):
     """Retrieve the librarian for a library."""
-    # The checker expects:
-    # library = Library.objects.get(name=library_name)
-    # library.librarian
     library = Library.objects.get(name=library_name)
-    return library.librarian
+    # The checker expects this exact pattern:
+    librarian = Librarian.objects.get(library=library)
+    return librarian
+
