2b90a69d56e30e50f6652963a832cd820738f3d5
yeabsiraamare <yeabsiraamaree@gmail.com>
2026-01-31 04:47:28 +0300

    Add custom permissions and secured book management views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -18,9 +18,12 @@ urlpatterns = [
     path('librarian-view/', librarian_view, name='librarian_view'),
     path('member-view/', member_view, name='member_view'),
 
-    path('add-book/', add_book, name='add_book'),
-    path('edit-book/<int:pk>/', edit_book, name='edit_book'), 
-    path('delete-book/<int:pk>/', delete_book, name='delete_book'),
+
+    path('add_book/', add_book, name='add_book'),
+    path('edit_book/<int:pk>/', edit_book, name='edit_book'),
+    path('delete_book/<int:pk>/', delete_book, name='delete_book'),
+
+
 
 
     
