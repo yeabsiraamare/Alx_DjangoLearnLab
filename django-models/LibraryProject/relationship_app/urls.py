7cf1b081881f91d083fa596a94982b2c39a95b6d
yeabsiraamare <yeabsiraamaree@gmail.com>
2026-01-31 04:26:53 +0300

    Add user authentication views, templates, and URLs

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,14 @@
 
 from django.urls import path
-from .views import login_view, logout_view, register_view, list_books, LibraryDetailView
+from django.contrib.auth.views import LoginView, LogoutView
+from . import views
 
 urlpatterns = [
-    path('login/', login_view, name='login'),
-    path('logout/', logout_view, name='logout'),
-    path('register/', register_view, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
 
-    # existing views
-    path('books/', list_books, name='list_books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    # your existing views
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,8 +3,11 @@ from django.views.generic.detail import DetailView
 from .models import Library
 from .models import Book
 from django.shortcuts import redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout
+from django.contrib.auth.forms import UserCreationForm 
+from django.contrib.auth import login
+
 
 
 
@@ -35,15 +38,19 @@ def logout_view(request):
     return render(request, 'relationship_app/logout.html')
 
 
-def register_view(request):
+
+
+
+def register(request):
     form = UserCreationForm()
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('list_books')
+            return redirect('login')
     return render(request, 'relationship_app/register.html', {'form': form})
+
 
 
 
