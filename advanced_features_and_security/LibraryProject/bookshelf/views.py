e6bbc9e6de452a9d3f140afa4878ec503c955df9
yeabsiraamare <yeabsiraamaree@gmail.com>
2026-02-01 19:41:40 +0300

    Add book_list view with permission checks and update README for permissions task

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -4,9 +4,9 @@ from .models import Book
 
 
 @permission_required('bookshelf.can_view', raise_exception=True)
-def view_books(request):
+def book_list(request):
     books = Book.objects.all()
-    return render(request, 'bookshelf/view_books.html', {'books': books})
+    return render(request, 'bookshelf/book_list.html', {'books': books})
 
 
 @permission_required('bookshelf.can_create', raise_exception=True)
