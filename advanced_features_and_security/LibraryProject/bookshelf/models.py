ea16dee5c387110b442ae21de99059fbdcfa6683
yeabsiraamare <yeabsiraamaree@gmail.com>
2026-02-01 19:39:22 +0300

    Add can_view, can_create, can_edit, can_delete permissions to Book model; update views with permission_required; add project README

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -2,14 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 
-# -----------------------------
-# Custom User Manager
-# -----------------------------
 class CustomUserManager(BaseUserManager):
     def create_user(self, username, email=None, password=None, **extra_fields):
         if not username:
             raise ValueError("Users must have a username")
-
         email = self.normalize_email(email)
         user = self.model(username=username, email=email, **extra_fields)
         user.set_password(password)
@@ -19,13 +15,9 @@ class CustomUserManager(BaseUserManager):
     def create_superuser(self, username, email=None, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
-
         return self.create_user(username, email, password, **extra_fields)
 
 
-# -----------------------------
-# Custom User Model
-# -----------------------------
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
@@ -36,13 +28,18 @@ class CustomUser(AbstractUser):
         return self.username
 
 
-# -----------------------------
-# Book Model (your original one)
-# -----------------------------
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     publication_year = models.IntegerField()
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
 
     def __str__(self):
         return f"{self.title} by {self.author} ({self.publication_year})"
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import permission_required
+from .models import Book
 
-# Create your views here.
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def view_books(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/view_books.html', {'books': books})
+
+
+@permission_required('bookshelf.can_create', raise_exception=True)
+def create_book(request):
+    return render(request, 'bookshelf/create_book.html')
+
+
+@permission_required('bookshelf.can_edit', raise_exception=True)
+def edit_book(request):
+    return render(request, 'bookshelf/edit_book.html')
+
+
+@permission_required('bookshelf.can_delete', raise_exception=True)
+def delete_book(request):
+    return render(request, 'bookshelf/delete_book.html')
