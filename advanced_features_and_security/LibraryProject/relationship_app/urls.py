d9bc02751d6b388ed4e93f8886340f6f669980e5
yeabsiraamare <yeabsiraamaree@gmail.com>
2026-02-01 19:17:41 +0300

    Refactor project to use CustomUser model; remove legacy UserProfile and role views; reset migrations

--- advanced_features_and_security/LibraryProject/relationship_app/urls.py
+++ advanced_features_and_security/LibraryProject/relationship_app/urls.py
@@ -1,19 +1,46 @@
+
 from django.urls import path
-from django.contrib.auth.views import LoginView, LogoutView
-from .views import add_book, edit_book, delete_book
+from django.contrib.auth import views as auth_views
 from . import views
+from .views import list_books, LibraryDetailView
+from .views import admin_view, librarian_view, member_view
+from .views import add_book, edit_book, delete_book
+from django.contrib.auth.views import LogoutView, LoginView
+
+'''urlpatterns = [
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+
+    path('register/', views.register, name='register'),
+    path(
+        'login/',
+        auth_views.LoginView.as_view(template_name='relationship_app/login.html'),
+        name='login'
+    ),
+    path(
+        'logout/',
+        auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'),
+        name='logout'
+    ),
+    path('admin-role/', admin_view, name='admin_view'),
+    path('librarian-role/', librarian_view, name='librarian_view'),
+    path('member-role/', member_view, name='member_view'),
+    path('add_book/', add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', edit_book, name='edit_book'),
+    path('delete_book/<int:book_id>/', delete_book, name='delete_book'),
+]'''
+
 
 urlpatterns = [
+    path('books/', views.list_books, name='list_books'),
+    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
-
-    # existing views
-    path('books/', views.list_books, name='list_books'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-
-    # Week 11 book permissions
-    path('add_book/', add_book, name='add_book'),
-    path('edit_book/<int:pk>/', edit_book, name='edit_book'),
-    path('delete_book/<int:pk>/', delete_book, name='delete_book'),
+    path('admin_view/', views.admin_view, name='admin_view'),
+    path('librarian_view/', views.librarian_view, name='librarian_view'),
+    path('member_view/', views.member_view, name='member_view'),
+    path('add_book/', views.add_book, name='add_book'),
+    path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),
+    path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),
 ]
