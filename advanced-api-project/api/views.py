55c789d3043b8f5d7b7e8710845394ed7218deb0
yeabsiraamare <yeabsiraamaree@gmail.com>
2026-02-14 16:13:17 +0300

    Add required permission imports and include API URLs in main project routing

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,8 @@
-from rest_framework import generics, permissions
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
+
 
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
