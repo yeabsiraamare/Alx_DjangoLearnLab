6cd44df23a0faa3fd970aa3061d376b48a0c779a
yeabsiraamare <yeabsiraamaree@gmail.com>
2026-02-15 12:13:05 +0300

    Integrate django-taggit and add tagging + search functionality with updated models, forms, views, and URLs

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,14 +1,17 @@
 from django import forms
+from taggit.forms import TagWidget
 from .models import Post, Comment
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ["title", "content", "tags"]
+        widgets = {
+            "tags": TagWidget(),
+        }
 
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ["content"]
-
