f96625f8245b7945d12e5d05af7753032d7eaf99
yeabsiraamare <yeabsiraamaree@gmail.com>
2026-02-22 21:42:04 +0300

    fix: ensure CharField and create_user in serializers

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -5,8 +5,8 @@ from rest_framework.authtoken.models import Token
 User = get_user_model()
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True)
-    password2 = serializers.CharField(write_only=True, required=True)
+    password = serializers.CharField(write_only=True, required=True)  # <-- explicit
+    password2 = serializers.CharField(write_only=True, required=True)  # <-- explicit
 
     class Meta:
         model = User
@@ -19,13 +19,6 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         validated_data.pop('password2')
-        # Explicitly use get_user_model().objects.create_user
         user = get_user_model().objects.create_user(**validated_data)
         Token.objects.create(user=user)
         return user
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ('id', 'username', 'email', 'bio', 'profile_picture', 'followers')
