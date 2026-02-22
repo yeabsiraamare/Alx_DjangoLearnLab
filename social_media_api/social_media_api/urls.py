9b210faec8a3c66ee86614208e9a1eadd8718b40
yeabsiraamare <yeabsiraamaree@gmail.com>
2026-02-22 22:17:12 +0300

    added api as a path

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('', include('posts.urls')),  # add posts routes
+    path('api/', include('posts.urls')),  # add posts routes
 ]
